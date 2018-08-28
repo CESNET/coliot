aa6ad1fa0038941de224c6b982298ce07a72a8b4
jalowiczor <jaluwka@gmail.com>
2018-08-28 16:24:33 +0200

    File loading changed.

--- superset/assets/src/modules/coliot.js
+++ superset/assets/src/modules/coliot.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
     var files = $('#unirec_file').val();
     var file_list = files.split(';');
     var modal = '';
-    for(i=0; i<file_list.length;i++){
-        modal = modal + '<tr> <td>'+modal+'</td> <td>root</td> <td>28.08.2018 11:21</td> <td>.md</td> <td>342 Kb</td> <td><p data-placement="top" data-toggle="tooltip" title="Edit"><div class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-path="'+modal+'"><span class="glyphicon glyphicon-open"></span></div></p></td> </tr>'
+    for(var i=0; i<file_list.length;i++){
+        modal = modal + '<tr> <td>'+file_list[i]+'</td> <td>root</td> <td>28.08.2018 11:21</td> <td>.md</td> <td>342 Kb</td> <td><p data-placement="top" data-toggle="tooltip" title="Edit"><div class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-path="'+file_list[i]+'"><span class="glyphicon glyphicon-open"></span></div></p></td> </tr>'
     }
     $("#file_name").append(modal);
 });
