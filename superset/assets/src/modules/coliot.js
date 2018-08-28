c229b472e55ed0119a3fe92f60b498d3c23a09f7
gre0071-pc <erik.g@seznam.cz>
2018-08-28 11:05:35 +0200

    add

--- superset/assets/src/modules/coliot.js
+++ superset/assets/src/modules/coliot.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#unirec_file').modal('hide');
+    $('#unirec_file').hide();
     $('.btn-success').click(function (){
         var btn = $(this).data();
         $('#unirec_file').val(btn.path);
